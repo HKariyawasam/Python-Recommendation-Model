458030a2ad638de505f1b3c2f106b0e47a51bf32
Kariyawasam H.G.P IT19973166 <it19973166@my.sliit.lk>
2023-05-14 16:54:01 +0530

    update

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -2,12 +2,39 @@ import pandas as pd
 from flask import jsonify
 from openpyxl import load_workbook
 
+import gdown
+
+# Shared Google Drive link of the Excel file
+shared_link = 'https://docs.google.com/spreadsheets/d/1xjEEOrjQkUBLDtb1UHGrurKkVdmp0meS/edit?usp=share_link&ouid=114060678686349206997&rtpof=true&sd=true'
+shared_link2 = 'https://docs.google.com/spreadsheets/d/18lfP1BZOfpy1S1Rjgrr1G7sy0JiknDQV/edit?usp=sharing&ouid=114060678686349206997&rtpof=true&sd=true'
+
+# Extract the file ID from the shared link
+file_id = shared_link.split('/')[-2]
+file_id2 = shared_link2.split('/')[-2]
+
+# Construct the download link for the file
+download_link = f'https://drive.google.com/uc?id={file_id}'
+download_link2 = f'https://drive.google.com/uc?id={file_id2}'
+
+# Download the Excel file
+file_path = 'file.xlsx'
+file_path2 = 'file2.xlsx'
+gdown.download(download_link, file_path, quiet=False)
+gdown.download(download_link, file_path2, quiet=False)
+
+
+
+
+
+
+
+
 def readActivityDataFrame():
-    activities_df = pd.read_excel('Python-Recommendation-Model/src/data/activities.xlsx')
+    activities_df = pd.read_excel(file_path2)
     return activities_df
 
 def readUsersCompletionDataFrame():
-    completerates_df = pd.read_excel('Python-Recommendation-Model/src/data/completerate.xlsx')
+    completerates_df = pd.read_excel(file_path)
     return completerates_df
 
 def updateUserCompletionRates(data):
