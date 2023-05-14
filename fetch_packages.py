a6349f95cc108e3b8364886c10dec364a461286c
Kariyawasam H.G.P IT19973166 <it19973166@my.sliit.lk>
2023-05-14 12:46:30 +0530

    update

--- fetch_packages.py
+++ fetch_packages.py
@@ -1,18 +1,5 @@
-from google.cloud import storage
 import subprocess
 
-# Create a client using the service account credentials
-client = storage.Client()
-
-# Get a reference to the bucket and the blob (packages.tar.gz file)
-bucket = client.get_bucket('parecengine')
-blob = bucket.blob('vendor/packages.tar.gz')
-
-# Download the packages.tar.gz file to a local directory
-blob.download_to_filename('/app/packages.tar.gz')
-
-# Extract the packages.tar.gz file
-subprocess.run(['tar', '-xzf', '/app/packages.tar.gz', '-C', '/app'])
-
-# Install the packages using pip
-subprocess.run(['pip', 'install', '--no-index', '--find-links', '/app/vendor/packages', '-r', 'requirements.txt'])
+# Install each package from the cloud storage bucket
+subprocess.check_call(['gsutil', '-m', 'cp', '-r', 'gs://parecengine/vendor', 'vendor'])
+subprocess.check_call(['pip', 'install', '--no-index', '--find-links', 'vendor', '-r', 'requirements.txt'])
